c7e9ff3c99213ffcb884bf76d76267c8fd457193
Rory Flynn <flynnro@goliath.ucdenver.pvt>
2019-11-26 12:29:04 -0700

    added my stuff

--- example_code2.py
+++ example_code2.py
@@ -9,7 +9,7 @@ import numpy as np
 from PIL import Image
 from tqdm import tqdm
 import os
-from PIL import Image 	
+from PIL import Image
 
 grph = tf.get_default_graph()
 sess = tf.Session(graph=grph)
@@ -26,14 +26,14 @@ from resizeimage import resizeimage
     #print("Note: not using Google CoLab")
     #COLAB = True
 
-# Generation resolution - Must be square 
+# Generation resolution - Must be square
 # Training data is also scaled to this.
 # Note GENERATE_RES higher than 4 will blow Google CoLab's memory.
 GENERATE_RES = 2 # (1=32, 2=64, 3=96, etc.)
 GENERATE_SQUARE = 32 * GENERATE_RES # rows/cols (should be square)
 IMAGE_CHANNELS = 3
 
-# Preview image 
+# Preview image
 PREVIEW_ROWS = 4
 PREVIEW_COLS = 7
 PREVIEW_MARGIN = 16
@@ -74,40 +74,40 @@ if not os.path.isfile(training_binary_path):
 else:
   print("Loading previous training pickle...")
   training_data = np.load(training_binary_path)
-  
+
 
 '''
 def build_generator(seed_size, channels):
 
 	model = Sequential()
-	
+
 	model.add(Dense(4*4*256,activation="relu",input_dim=seed_size))
 	model.add(Reshape((4,4,256)))
-	
+
 	model.add(UpSampling2D())
 	model.add(Conv2D(256,kernel_size=3,padding="same"))
 	model.add(BatchNormalization(momentum=0.8))
 	model.add(Activation("relu"))
-	
+
 	model.add(UpSampling2D())
 	model.add(Conv2D(256,kernel_size=3,padding="same"))
 	model.add(BatchNormalization(momentum=0.8))
 	model.add(Activation("relu"))
-	
+
 	# Output resolution, additional upsampling
 	for i in range(GENERATE_RES):
 		model.add(UpSampling2D())
 		model.add(Conv2D(128,kernel_size=3,padding="same"))
 		model.add(BatchNormalization(momentum=0.8))
 		model.add(Activation("relu"))
-	
+
 	# Final CNN layer
 	model.add(Conv2D(channels,kernel_size=3,padding="same"))
 	model.add(Activation("tanh"))
-	
+
 	input = Input(shape=(seed_size,))
 	generated_image = model(input)
-	
+
 	return Model(input,generated_image, name = "Generator")
 
 '''
@@ -119,9 +119,45 @@ def noise_enc():
 ###Rory code
 
 def context_enc():
-
+    # one aproch
+    # https://towardsdatascience.com/an-approach-towards-convolutional-recurrent-neural-networks-f54cbeecd4a6
+    # vary setings
+    shape_in = int(320), int(8), int(1)
+    shape_out = int(8134), int(120)
+    dropoutrate = 0.3
+    x_start = Input(shape=(shape_in))
+    x = x_start
+    for _i, _cnt in enumerate((2, 2)):
+        x = Conv2D(filters = 100, kernel_size=(2, 2), padding='same',)(x)
+        x = BatchNormalization(axis=1)(x)
+        x = Activation('relu')(x)
+        x = LeakyReLU()(x)
+        #x = MaxPooling2D(pool_size=(2,2), dim_ordering="th" )(x)
+        x = MaxPooling2D(pool_size=2)(x)
+        spec_x = Dropout(dropoutrate)(x)
+    x = Permute((2, 1, 3))(x)
+    x = Reshape((1, -1))(x)
+    # The Gru/recurrent portion
+    # Get some knowledge
+    # http://colah.github.io/posts/2015-08-Understanding-LSTMs/
+    for r in (10,10):
+        x = Bidirectional(
+                GRU(r,
+                    activation='tanh',
+                    dropout=dropoutrate,
+                    recurrent_dropout=dropoutrate,
+                    return_sequences=True),
+                merge_mode='concat')(x)
+        for f in ((2,2)):
+            x = TimeDistributed(Dense(f))(x)
+    x = Dropout(dropoutrate)(x)
+    x = TimeDistributed(Dense(shape_out[-1]))(x)
+    out = Activation('sigmoid', name='strong_out')(x)
+    audio_context = Model(inputs=x_start, outputs=out)
+    audio_context.compile(optimizer='Adam', loss='binary_crossentropy',metrics = ['accuracy'])
+    ce = audio_context
     return ce
-    
+
 #UNET Functions
 
 def down_block(x, filters, kernal_size = (3, 3), padding ='same', strides=1):
@@ -197,44 +233,44 @@ def build_discriminator(image_shape):
 	model = Sequential()
 	model.add(Conv2D(32, kernel_size=3, strides=2, input_shape=image_shape, padding="same"))
 	model.add(LeakyReLU(alpha=0.2))
-	
+
 	model.add(Dropout(0.25))
 	model.add(Conv2D(64, kernel_size=3, strides=2, padding="same"))
 	model.add(ZeroPadding2D(padding=((0,1),(0,1))))
 	model.add(BatchNormalization(momentum=0.8))
 	model.add(LeakyReLU(alpha=0.2))
-	
+
 	model.add(Dropout(0.25))
 	model.add(Conv2D(128, kernel_size=3, strides=2, padding="same"))
 	model.add(BatchNormalization(momentum=0.8))
 	model.add(LeakyReLU(alpha=0.2))
-	
+
 	model.add(Dropout(0.25))
 	model.add(Conv2D(256, kernel_size=3, strides=1, padding="same"))
 	model.add(BatchNormalization(momentum=0.8))
 	model.add(LeakyReLU(alpha=0.2))
-	
+
 	model.add(Dropout(0.25))
 	model.add(Conv2D(512, kernel_size=3, strides=1, padding="same"))
 	model.add(BatchNormalization(momentum=0.8))
 	model.add(LeakyReLU(alpha=0.2))
-	
+
 	model.add(Dropout(0.25))
 	model.add(Flatten())
 	model.add(Dense(1, activation='sigmoid'))
 	model.add(Reshape((64, 64, 3)))
 	input_image = Input(shape=image_shape)
-	
+
 	validity = model(input_image)
-	
+
 	return Model(input_image, validity, name = "Discriminator")
-  
+
 def save_images(cnt,noise):
-  image_array = np.full(( 
-      PREVIEW_MARGIN + (PREVIEW_ROWS * (GENERATE_SQUARE+PREVIEW_MARGIN)), 
-      PREVIEW_MARGIN + (PREVIEW_COLS * (GENERATE_SQUARE+PREVIEW_MARGIN)), 3), 
+  image_array = np.full((
+      PREVIEW_MARGIN + (PREVIEW_ROWS * (GENERATE_SQUARE+PREVIEW_MARGIN)),
+      PREVIEW_MARGIN + (PREVIEW_COLS * (GENERATE_SQUARE+PREVIEW_MARGIN)), 3),
       255, dtype=np.uint8)
-  
+
   generated_images = generator.predict(noise)
 
   generated_images = 0.5 * generated_images + 0.5
@@ -247,11 +283,11 @@ def save_images(cnt,noise):
         image_array[r:r+GENERATE_SQUARE,c:c+GENERATE_SQUARE] = generated_images[image_count] * 255
         image_count += 1
 
-          
+
   output_path = os.path.join(DATA_PATH,'output')
   if not os.path.exists(output_path):
     os.makedirs(output_path)
-  
+
   filename = os.path.join(output_path,f"train-{cnt}.png")
   im = Image.fromarray(image_array)
   im.save(filename)
@@ -288,24 +324,24 @@ for epoch in range(EPOCHS):
     # Generate some images
     seed = np.random.normal(0,1,(BATCH_SIZE,SEED_SIZE))
     x_fake = generator.predict(seed)
-    
+
     print("xxxx")
     print(x_real.shape)
     print(y_real.shape)
     # Train discriminator on real and fake
-    
+
     discriminator_metric_real = discriminator.train_on_batch(x_real,y_real)
     discriminator_metric_generated = discriminator.train_on_batch(x_fake,y_fake)
     discriminator_metric = 0.5 * np.add(discriminator_metric_real,discriminator_metric_generated)
-    
+
     # Train generator on Calculate losses
     generator_metric = combined.train_on_batch(seed,y_real)
-    
+
     # Time for an update?
     if epoch % SAVE_FREQ == 0:
         save_images(cnt, fixed_seed)
         cnt += 1
         print(f"Epoch {epoch}, Discriminator accuarcy: {discriminator_metric[1]}, Generator accuracy: {generator_metric[1]}")
-        
+
 generator.save(os.path.join(DATA_PATH,"face_generator.h5"))
 

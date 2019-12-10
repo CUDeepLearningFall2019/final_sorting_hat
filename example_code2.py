bfcf38b254d68f6d457f23bf5a9433f87db3bf80
Rory Flynn <flynnro@ucdenver.edu>
2019-12-09 23:28:38 -0700

    before clean

--- example_code2.py
+++ example_code2.py
@@ -21,7 +21,7 @@ from tensorflow.keras.layers import TimeDistributed
 from tensorflow.keras.layers import UpSampling2D
 from tensorflow.keras.layers import ZeroPadding2D
 # load model
-from tensorflow.keras import models
+from tensorflow.keras import Model
 from tensorflow.keras.models import Sequential, load_model, Model
 # load back end
 from tensorflow.keras import backend
@@ -35,65 +35,53 @@ import numpy as np
 from PIL import Image
 from tqdm import tqdm
 
-grph = tf.get_default_graph()
-sess = tf.Session(graph=grph)
-
-# Generation resolution - Must be square
-# Training data is also scaled to this.
-# Note GENERATEoRES higher than 4 will blow Google CoLab's memory.
-GENERATE_RES = 2 # (1=32, 2=64, 3=96, etc.)
-GENERATE_SQUARE = 32 * GENERATE_RES # rows/cols (should be square)
-HEIGHT = 720
-WIDTH = 1280
-IMAGE_CHANNELS = 3
-INPUT_SHAPE = (HEIGHT, WIDTH)
-
-# Preview image
-PREVIEW_ROWS = 4
-PREVIEW_COLS = 7
-PREVIEW_MARGIN = 16
-SAVE_FREQ = 100
-
-# Size vector to generate images from
-SEED_SIZE = 10
-
-# Configuration
-DATA_PATH = ''
-EPOCHS = 10
-BATCH_SIZE = 64
-
-#print(f"Will generate {GENERATE_SQUARE}px square images.")
-
-# Image set has 11,682 images.  Can take over an hour for initial preprocessing.
-# Because of this time needed, save a Numpy preprocessed file.
-# Note, that file is large enough to cause problems for sume verisons of Pickle,
-# so Numpy binary files are used.
-#training_vid_path = os.path.join(DATA_PATH,f'training_vid_{WIDTH}_{HEIGHT}.npy')
-training_vid_path = "../data/Video/video1.npy"
-training_audio_path = "../data/Audio/audio1.npy"
-
-print(f"Looking for file: {training_vid_path}")
-
-if not os.path.isfile(training_vid_path):
-  print("Loading training images...")
-  training_vid = []
-  faces_path = os.path.join(DATA_PATH,'frames_test')
-  for filename in tqdm(os.listdir(faces_path)):
-      path = os.path.join(faces_path,filename)
-      image = Image.open(path).resize(INPUT_SHAPE,Image.ANTIALIAS)
-      training_vid.append(np.asarray(image))
-  training_vid = np.reshape(training_vid,(-1,INPUT_SHAPE,IMAGE_CHANNELS))
-  training_vid = training_vid / 127.5 - 1.
-  print("Saving training image binary...")
-  np.save(training_vid_path,training_vid)
-else:
-  print("Loading previous training pickle...")
-  training_vid = np.load(training_vid_path)
-  training_aud = np.load(training_audio_path)
-
-training_vid.shape
-training_aud.shape
-training_vid[0,:,:,:]
+# grph = tf.get_default_graph()
+# sess = tf.Session(graph=grph)
+#
+# # Generation resolution - Must be square
+# # Training data is also scaled to this.
+# # Note GENERATEoRES higher than 4 will blow Google CoLab's memory.
+# GENERATE_RES = 2 # (1=32, 2=64, 3=96, etc.)
+# GENERATE_SQUARE = 32 * GENERATE_RES # rows/cols (should be square)
+# HEIGHT = 720
+# WIDTH = 1280
+# IMAGE_CHANNELS = 3
+# INPUT_SHAPE = (HEIGHT, WIDTH)
+#
+# # Preview image
+# PREVIEW_ROWS = 4
+# PREVIEW_COLS = 7
+# PREVIEW_MARGIN = 16
+# SAVE_FREQ = 100
+#
+# # Size vector to generate images from
+# SEED_SIZE = 10
+#
+# # Configuration
+# DATA_PATH = ''
+# EPOCHS = 10
+# BATCH_SIZE = 64
+#
+# #print(f"Will generate {GENERATE_SQUARE}px square images.")
+#
+# # Image set has 11,682 images.  Can take over an hour for initial preprocessing.
+# # Because of this time needed, save a Numpy preprocessed file.
+# # Note, that file is large enough to cause problems for some versions of Pickle,
+# # so Numpy binary files are used.
+# training_vid_path = "/home/dl-group/data/Video/video1.npy"
+# training_audio_path = "/home/dl-group/data/Audio/audio1.npy"
+#
+# print("Loading data pickle...")
+# training_vid = np.load(training_vid_path)
+# training_aud = np.load(training_audio_path)
+#
+# # take the first video and make a matrix used as part of generator context
+# first_vid = np.zeros(training_vid.shape, dtype='float16')
+# for i in range(first_vid.shape[0]):
+#     first_vid[i,:,:,:] = training_vid[0,:,:,:]
+#
+# training_vid.shape
+# training_aud.shape
 
 def noise_enc():
     ne = None
@@ -115,7 +103,7 @@ def context_enc():
 
     for _i, _cnt in enumerate((2, 2)):
         x = Conv2D(filters = 100, kernel_size=(2, 2), padding='same',)(x)
-        x = BatchNormalization(axis=1)(x)
+        x = BatchNormalization(axis=-1)(x)
         #x = Activation('relu')(x)
         x = LeakyReLU()(x)
         #x = MaxPooling2D(pool_size=(2,2), dim_ordering="th" )(x)
@@ -151,6 +139,10 @@ def context_enc():
     #ce = audio_context
     return ce, x_start
 
+# Q0 can I flatten and Reshape and still get good results
+# Q1 if I do that were should my GRU layers go
+# Q3 how do I ensure that the down block matches the shape of the up block
+# Q4
 
 #UNET Functions
 
@@ -168,13 +160,23 @@ def up_block(x, skip, filters, kernal_size =(3, 3), padding='same', strides= 1):
     return c
 
 def up_block1(x, skip, filters, kernal_size =(3, 3), padding='same', strides= 1):
+    us = UpSampling2D((2, 2))(x)
     concat = Concatenate()([x, skip])
     c= Conv2D(filters, kernal_size, padding=padding, strides=strides, activation='relu')(concat)
     c= Conv2D(filters, kernal_size, padding=padding, strides=strides, activation='relu')(c)
     return c
 
 def bottleneck(IE, AE, NE, filters, kernal_size = (3, 3), padding ='same', strides=1):
+    #join_shape = (22,40, 128)
+    join_shape = (4, 8, 128)
+
+    IE = Flatten()(IE)
+    AE = Flatten()(AE)
     concat1 = Concatenate()([IE, AE])
+    concat1 = Dense(np.prod(join_shape))(concat1)
+    concat1 = Reshape(join_shape)(concat1)
+
+
     #concat1 = Concatenate()([p5, context_enc()])
     #filters = f[5]
     #because we are not using gausian noise, I have commented out that concatentation and only passed the first concat
@@ -185,16 +187,18 @@ def bottleneck(IE, AE, NE, filters, kernal_size = (3, 3), padding ='same', strid
     #Flatten()(c)
     #c = Dense((45, 80, 63))(c)
     # fix this
-    c = Reshape((45, 80, 63))(c)
+    # c = Reshape((45, 80, 63))(c)
+    #c = concat1
     return c
 
 #def Gener(input_dim, image_channels):
-def Gener():
+#def build_generator(discrim):
+def build_generator():
     f= [8, 16, 32, 64, 128, 256]
     #Filters per layers
 
     #Data shape entering the convolusion
-    inputs = Input((720,1280,3))
+    inputs = Input((64,128,3))
 
 
     #Input layer
@@ -205,7 +209,10 @@ def Gener():
     c2, p2 = down_block(p1, f[1])
     c3, p3 = down_block(p2, f[2])
     c4, p4 = down_block(p3, f[3])
-    c5, p5 = down_block(p4, f[4])
+    c5, p5 = down_block(p4, f[4]) # output is 4,8, 128
+    # test = Model(inputs=inputs, outputs=p5)
+    # test.compile(optimizer='Adam', loss='binary_crossentropy',metrics = ['accuracy'])
+    # test.summary()
 
     #switch over layer to up bloc decoder
     ne = noise_enc()
@@ -226,47 +233,11 @@ def Gener():
 
     #Keras model output
     model = Model([inputs, inputs2], outputs, name='gener')
+    #model.compile(optimizer='Adam', loss='binary_crossentropy',metrics = ['accuracy'])
 
-    return model
+    return model, [inputs, inputs2]
 
 
-
-def build_discriminator(image_shape= (720,1280,3)):
-
-    model = Sequential()
-    model.add(Conv2D(32, kernel_size=3, strides=2, input_shape=image_shape, padding="same"))
-    model.add(LeakyReLU(alpha=0.2))
-
-    model.add(Dropout(0.25))
-    model.add(Conv2D(64, kernel_size=3, strides=2, padding="same"))
-    model.add(ZeroPadding2D(padding=((0,1),(0,1))))
-    model.add(BatchNormalization(momentum=0.8))
-    model.add(LeakyReLU(alpha=0.2))
-
-    model.add(Dropout(0.25))
-    model.add(Conv2D(128, kernel_size=3, strides=2, padding="same"))
-    model.add(BatchNormalization(momentum=0.8))
-    model.add(LeakyReLU(alpha=0.2))
-
-    model.add(Dropout(0.25))
-    model.add(Conv2D(256, kernel_size=3, strides=1, padding="same"))
-    model.add(BatchNormalization(momentum=0.8))
-    model.add(LeakyReLU(alpha=0.2))
-
-    model.add(Dropout(0.25))
-    model.add(Conv2D(512, kernel_size=3, strides=1, padding="same"))
-    model.add(BatchNormalization(momentum=0.8))
-    model.add(LeakyReLU(alpha=0.2))
-
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(1, activation='sigmoid'))
-    model.add(Reshape((64, 64, 3)))
-    input_image = Input(shape=image_shape)
-
-    validity = model(input_image)
-
-    return Model(input_image, validity, name = "Discriminator")
 
 def build_discriminator():
     # one aproch
@@ -274,8 +245,8 @@ def build_discriminator():
 
     # vary setings
     shape_in_x = int(320), int(8), int(1)
-    shape_in_y = int(720), int(1280), int(3)
-    shape_out = int(8134), int(120)
+    shape_in_y = int(64), int(128), int(3)
+    #shape_out = int(8134), int(120)
     dropoutrate = 0.3
 
     x_start = Input(shape=(shape_in_x))
@@ -283,15 +254,16 @@ def build_discriminator():
 
     for _i, _cnt in enumerate((2, 2)):
         x = Conv2D(filters = 100, kernel_size=(2, 2), padding='same',)(x)
-        x = BatchNormalization(axis=1)(x)
+        x = BatchNormalization(axis=-1)(x)
         #x = Activation('relu')(x)
         x = LeakyReLU()(x)
         #x = MaxPooling2D(pool_size=(2,2), dim_ordering="th" )(x)
         x = MaxPooling2D(pool_size=2)(x)
         x = Dropout(dropoutrate)(x)
 
-    x = Permute((2, 1, 3))(x)
-    x = Reshape((1, 16000))(x)
+    x = Flatten()(x) # shape out = none 16000
+    #x = Permute((2, 1, 3))(x)
+    #x = Reshape((1, 16000))(x)
 
     #out = Activation('sigmoid', name='strong_out')(x)
     #audio_context = Model(inputs=x_start, outputs=out)
@@ -303,27 +275,28 @@ def build_discriminator():
 
     for _i, _cnt in enumerate((2, 2)):
         y = Conv2D(filters = 100, kernel_size=(2, 2), padding='same',)(y)
-        y = BatchNormalization(axis=1)(y)
+        y = BatchNormalization(axis=-1)(y)
         #y = Activation('relu')(y)
         y = LeakyReLU()(y)
         #y = MayPooling2D(pool_size=(2,2), dim_ordering="th" )(y)
         y = MaxPooling2D(pool_size=2)(y)
         y = Dropout(dropoutrate)(y)
 
-    y = Permute((2, 1, 3))(y)
-    y = Reshape((1, 5760000))(y)
+    y = Flatten()(y)
+    #y = Permute((2, 1, 3))(y)
+    #y = Reshape((1, 5760000))(y)
     #out = Activation('sigmoid', name='strong_out')(y)
     #audio_context = Model(inputs=y_start, outputs=out)
     #audio_context.compile(optimizer='Adam', loss='binary_crossentropy',metrics = ['accuracy'])
-    #audio_context.summary()
+    #audio_context.summary()# (None, 51200)
 
-    z = Concatenate()([x, y])
+    z = Concatenate()([y, x])
+    #z = Reshape((-1, 1))(z) # Why dose this not work?
+    z = Reshape((67200, 1))(z)
     #out = Activation('sigmoid', name='strong_out')(z)
     #audio_context = Model(inputs=[x_start,y_start], outputs=out)
     #audio_context.compile(optimizer='Adam', loss='binary_crossentropy',metrics = ['accuracy'])
-    #audio_context.summary()
-
-
+    #audio_context.summary() # (None, 67200)
 
     # The Gru/recurrent portion
     # Get some knowledge
@@ -347,83 +320,119 @@ def build_discriminator():
     z = Dense(1,activation = 'sigmoid')(z)
 
     #out = Activation('sigmoid', name='strong_out')(z)
-    descrim = Model(inputs=[x_start, y_start], outputs=z)
+    descrim = Model(inputs=[y_start, x_start], outputs=z)
     descrim.compile(optimizer='Adam', loss='binary_crossentropy',metrics = ['accuracy'])
     #descrim.summary()
     #ce = audio_context
-    return descrim
+    return descrim, x_start
 
 
-image_shape = (INPUT_SHAPE, IMAGE_CHANNELS)
-optimizer = Adam(1.5e-4,0.5) # learning rate and momentum adjusted from paper
 
-discriminator = build_discriminator()
-#discriminator.trainable = False
-#discriminator.compile(loss="binary_crossentropy",optimizer=optimizer,metrics=["accuracy"])
-#gener = Gener(INPUT_SHAPE,IMAGE_CHANNELS)
-gener = Gener()
-gener.summary()
-discriminator.summary()
+discriminator, aud_input = build_discriminator()
+#discriminator.summary()
+generator, inputs = build_generator()
+gd_joint = generator(inputs)
+#Keras model output
+#model = Model([inputs, inputs2], outputs, name='gener')
+#model.compile(optimizer='Adam', loss='binary_crossentropy',metrics = ['accuracy'])
+
+# For the combined model we will only train the generator
+discriminator.trainable = False
+
+# The discriminator takes generated images as input and determines validity
+validity = discriminator([gd_joint, aud_input])
+
+# The combined model  (stacked generator and discriminator)
+# Trains the generator to fool the discriminator
+inputs.append(aud_input)
+combination = Model(inputs, validity)
+combination.compile(loss='binary_crossentropy', optimizer=Adam(0.0002, 0.5))
+
+
+# Image set has 11,682 images.  Can take over an hour for initial preprocessing.
+# Because of this time needed, save a Numpy preprocessed file.
+# Note, that file is large enough to cause problems for sume verisons of Pickle,
+# so Numpy binary files are used.
+training_vid_path = "/home/dl-group/data/Video/video1.npy"
+training_audio_path = "/home/dl-group/data/Audio/audio1.npy"
+
+print("Loading training data.")
+training_vid = np.load(training_vid_path)
+training_aud = np.load(training_audio_path)
+#training_vid = np.load(mini_vid_path)
+#training_aud = np.load(mini_audio_path)
+# take the first video and make a matrix used as part of generator context
+first_vid = np.zeros(training_vid.shape, dtype='float16')
+for i in range(first_vid.shape[0]):
+    first_vid[i,:,:,:] = training_vid[0,:,:,:]
+
+training_vid.shape
+training_aud.shape
 
 #
 # Training block
 #
 
-iter = 10
-BATCH_SIZE = 2
-save_dir = "./gener_output"
-start = 0
-for step in range(iter):
-    rand_lat = np.random.normal(size=(BATCH_SIZE, 720))
-    generated_images = gener.predict([image[], audio])
-    stop = start+BATCH_SIZE
-    real_image = [training_vid[start:stop], training_aud[start:stop]]
-    combined_image =
-
-
-
-#random_input = Input(shape=(SEED_SIZE,))
-
-generated_image = gener()
-
-validity = discriminator(generated_image)
-combined = Model(random_input,validity)
-combined.compile(loss="binary_crossentropy",optimizer=optimizer,metrics=["accuracy"])
-
-y_real = np.ones((64, BATCH_SIZE, BATCH_SIZE, 3))
-y_real = y_real.reshape((-1, 64, 64, 3))
-
-y_fake = np.zeros((BATCH_SIZE,1))
-
-fixed_seed = np.random.normal(0, 1, (PREVIEW_ROWS * PREVIEW_COLS, SEED_SIZE))
+#iter = 10
+#BATCH_SIZE = 2
+#save_dir = "./gener_output"
+#start = 0
+#for step in range(iter):
+#rand_lat = np.random.normal(size=(BATCH_SIZE, 64))
+#generated_images = generator.predict([first_vid[:10],training_aud[:10]])
+#stop = start+BATCH_SIZE
+#real_image = [training_vid[start:stop], training_aud[start:stop]]
+#0combined_image =
+#
+#
+#
+##random_input = Input(shape=(SEED_SIZE,))
+#
+#generated_image = gener()
+#
+#validity = discriminator(generated_image)
+#combined = Model(random_input,validity)
+#combined.compile(loss="binary_crossentropy",optimizer=optimizer,metrics=["accuracy"])
+#
+#y_real = np.ones((64, BATCH_SIZE, BATCH_SIZE, 3))
+#y_real = y_real.reshape((-1, 64, 64, 3))
+#
+#y_fake = np.zeros((BATCH_SIZE,1))
+#
+#fixed_seed = np.random.normal(0, 1, (PREVIEW_ROWS * PREVIEW_COLS, SEED_SIZE))
 
 cnt = 1
-for epoch in range(EPOCHS):
-    idx = np.random.randint(0,training_vid.shape[0],BATCH_SIZE)
-    x_real = training_vid[idx]
-    x_real = x_real.reshape((-1, 64, 64, 3))
+num_epoch = 10
+batch_size = 10
+seed_size = 42
+work_path = './'
+save_freq = num_epoch - 1
 
+for epoch in range(num_epoch):
+    idi = np.random.randint(0, training_vid.shape[0]-batch_size)
+    idx = list(range(idi, idi + batch_size))
+    x_real_vid = training_vid[idx]
+    x_real_aud = training_aud[idx]
+    image_context = first_vid[idx]
     # Generate some images
-    seed = np.random.normal(0,1,(BATCH_SIZE,SEED_SIZE))
-    x_fake = gener.predict(seed)
-
+    # seed = np.random.normal(0,1,(batch_size,seed_size))
+    x_fake_vid = generator.predict([image_context, x_real_aud])
+    y_real = np.ones((batch_size))
+    y_fake = np.zeros((batch_size))
     print("xxxx")
     print(x_real.shape)
     print(y_real.shape)
     # Train discriminator on real and fake
-
-    discriminator_metric_real = discriminator.train_on_batch(x_real,y_real)
-    discriminator_metric_generated = discriminator.train_on_batch(x_fake,y_fake)
+    discriminator_metric_real = discriminator.train_on_batch([x_real_vid, x_real_aud], y_real)
+    discriminator_metric_generated = discriminator.train_on_batch([x_fake_vid, x_real_aud], y_fake)
     discriminator_metric = 0.5 * np.add(discriminator_metric_real,discriminator_metric_generated)
-
     # Train generator on Calculate losses
-    generator_metric = combined.train_on_batch(seed,y_real)
-
+    # y_pred = discriminator.predict([x_real_aud, x_fake_vid])
+    generator_metric = combination.train_on_batch([image_context, x_real_aud, x_real_aud], y_pred)
     # Time for an update?
-    if epoch % SAVE_FREQ == 0:
+    if epoch % save_freq == 0:
         save_images(cnt, fixed_seed)
         cnt += 1
         print(f"Epoch {epoch}, Discriminator accuarcy: {discriminator_metric[1]}, Generator accuracy: {generator_metric[1]}")
-
-gener.save(os.path.join(DATA_PATH,"face_generator.h5"))
+    generator.save(os.path.join(work_path,"face_generator.h5"))
 
